abd2354ab2d56acfae24a252c1eb733b279db27e
maple <kirby741852963@gmail.com>
2018-03-25 17:48:56 +0800

    Change openurl to opn in system downloader (#12)

--- src/downloaders/system.js
+++ src/downloaders/system.js
@@ -1,4 +1,4 @@
-const { open } = require('openurl')
+const open = require('opn')
 const log = require('debug')('dmhy:downloaders:system')
 const { console, l10n } = require('../utils')
 
@@ -7,11 +7,9 @@ const thread = JSON.parse(args[0])
 
 log(args)
 
-try {
-  open(thread.link, () => {
-    console.success(l10n('CLIENT_DL_SUCCESS_MSG', { title: thread.title }))
-  })
-} catch (err) {
+open(thread.link).then(() => {
+  console.success(l10n('CLIENT_DL_SUCCESS_MSG', { title: thread.title }))
+}).catch(err => {
   console.error(l10n('CLIENT_DL_FAILED_MSG', { title: thread.title }))
   console.error('Failed to start subprocess. %o', err)
-}
+})
