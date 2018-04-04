24358d5842a07be2b7a580d688bb5eb272137101
FlandreDaisuki <vbnm123c@gmail.com>
2018-04-04 11:23:46 +0800

    Fix some descriptions

--- bin/command/add.js
+++ bin/command/add.js
@@ -1,5 +1,5 @@
 const { l10n, consola } = require('../..');
-exports.command = 'add [subscribables...]';
+exports.command = 'add [subscribable...]';
 
 exports.aliases = [];
 
@@ -39,9 +39,9 @@ exports.builder = (yargs) => {
       consola.error(msg);
       process.exit(1);
     })
-    .example('$0 add "搖曳露營,萌喵,繁體"', '使用訂閱用字串')
-    .example('$0 add "./camp.yml"', '使用訂閱設定檔')
-    .example('$0 add -i', '使用互動式界面');
+    .example('$0 add "搖曳露營,萌喵,繁體,~1080p~"', l10n('CMD_ADD_EXAMPLE1_DESC'))
+    .example('$0 add "./camp.yml"', l10n('CMD_ADD_EXAMPLE2_DESC'))
+    .example('$0 add -i', l10n('CMD_ADD_EXAMPLE3_DESC'));
 };
 
 exports.handler = (argv) => {
