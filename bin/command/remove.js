67a85980b06aaf960178fd99b1c5ce2a66d41a86
FlandreDaisuki <vbnm123c@gmail.com>
2018-04-06 17:24:02 +0800

    CLI command: remove done

--- bin/command/remove.js
+++ bin/command/remove.js
@@ -1,16 +1,48 @@
-const { l10n, print } = require('../..');
-exports.command = 'remove <SID...>';
+const { l10n, print, Database } = require('../..');
+exports.command = 'remove [SID...]';
 
 exports.aliases = ['rm'];
 
 exports.desc = l10n('CMD_RM_DESC');
 
 exports.builder = (yargs) => {
-  // TODO
+  yargs
+    .usage(l10n('CMD_RM_USAGE'))
+    .options({
+      'a': {
+        alias: 'all',
+        describe: l10n('CMD_RM_OPT_A'),
+        type: 'boolean',
+      },
+    })
+    .check((argv) => {
+      if (!argv.SID && !argv.all) {
+        throw new Error(l10n('CMD_RM_OPT_NO_A_NO_SID'));
+      }
+      return true;
+    })
+    .example('$0 rm AAA', l10n('CMD_RM_EXAMPLE1_DESC'));
 };
 
 exports.handler = (argv) => {
-  // TODO
-  print.log(JSON.stringify(argv, null, 2));
+  const db = new Database();
+
+  if (argv.all) {
+    const sids = db.subscriptions.map((sub) => sub.sid);
+    sids.forEach((sid) => {
+      const removed = db.remove(sid);
+      if (removed) {
+        print.success(l10n('CMD_RM_REMOVE_SUCCESS', { title: removed.title }));
+      }
+    });
+  } else {
+    argv.SID.forEach((sid) => {
+      const removed = db.remove(sid);
+      if (removed) {
+        print.success(l10n('CMD_RM_REMOVE_SUCCESS', { title: removed.title }));
+      }
+    });
+  }
+  db.save();
   process.exit(0);
 };
