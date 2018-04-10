fd52cfbf7f6359e662b0b0f5fc4d225521fb3eff
FlandreDaisuki <vbnm123c@gmail.com>
2018-04-10 09:47:04 +0800

    Fix preinstall.js

--- npm-scripts/preinstall.js
+++ npm-scripts/preinstall.js
@@ -3,18 +3,20 @@ const fs = require('fs-extra');
 const { CONST } = require('..');
 const { defaultProjectDataDir } = CONST;
 
-const db = fs.readJSONSync(`${defaultProjectDataDir}/fakedb.json`);
 const ss = []; // subscribable string
 
-if (Array.isArray(db)) {
-  // 0.3.x
-  for (const sub of db) {
-    ss.push([sub.name, ...sub.keywords].join(','));
-  }
-} else if (db.subscriptions) {
-  // 0.5.x
-  for (const sub of db.subscriptions) {
-    ss.push([sub.name, ...sub.keywords].join(','));
+if (fs.existsSync(`${defaultProjectDataDir}/fakedb.json`)) {
+  const db = fs.readJSONSync(`${defaultProjectDataDir}/fakedb.json`);
+  if (Array.isArray(db)) {
+    // 0.3.x
+    for (const sub of db) {
+      ss.push([sub.name, ...sub.keywords].join(','));
+    }
+  } else if (db.subscriptions) {
+    // 0.5.x
+    for (const sub of db.subscriptions) {
+      ss.push([sub.name, ...sub.keywords].join(','));
+    }
   }
 }
 
