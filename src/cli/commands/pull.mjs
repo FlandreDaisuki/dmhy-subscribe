b2d37284c361c783abfee8ae1422d5831a7a3762
FlandreDaisuki <vbnm123c+github@gmail.com>
2023-02-26 15:59:08 +0800

    feat: support `sid` to `pull` command

--- src/cli/commands/pull.mjs
+++ src/cli/commands/pull.mjs
@@ -8,7 +8,7 @@ import {
 import * as logger from '../../logger.mjs';
 import { getRssListByKeywords, parsePattern } from '../../utils.mjs';
 
-export const command = 'pull';
+export const command = 'pull [sid..]';
 
 export const describe = 'pull the remote threads by your subscriptions';
 
@@ -23,32 +23,39 @@ export const handler = async(argv) => {
 
   try {
     const subscriptions = await getAllSubscriptions();
+    const pullingSids = (argv.sid ?? []).map((s) => String(s).toLowerCase());
     await Promise.all(
-      subscriptions.map(async(sub) => {
-        debug('dmhy:cli:pull:subscription')(sub);
+      subscriptions
+        .filter((sub) => {
+          if (pullingSids.length === 0) { return true; }
 
-        const rss = await getRssListByKeywords(sub.keywords);
-        const getExcludePattern = () => parsePattern(String(sub.excludePattern));
-        const filteredRssItems = rss.items.filter((item) => !(getExcludePattern().test(item.title)));
+          return pullingSids.includes(String(sub.sid).toLowerCase());
+        })
+        .map(async(sub) => {
+          debug('dmhy:cli:pull:subscription')(sub);
 
-        return Promise.all(filteredRssItems.map(async(rssItem) => {
-          const dmhyLink = rssItem.link;
-          const magnet = rssItem.enclosure.url;
-          const title = rssItem.title;
-          const publishDate = rssItem.isoDate;
-          if (await isExistingThreadDmhyLink(dmhyLink)) {
-            return;
-          }
+          const rss = await getRssListByKeywords(sub.keywords);
+          const getExcludePattern = () => parsePattern(String(sub.excludePattern));
+          const filteredRssItems = rss.items.filter((item) => !(getExcludePattern().test(item.title)));
 
-          const threadResult = await createThread(dmhyLink, magnet, title, publishDate);
-          debug('dmhy:cli:pull:threadResult')(threadResult);
+          return Promise.all(filteredRssItems.map(async(rssItem) => {
+            const dmhyLink = rssItem.link;
+            const magnet = rssItem.enclosure.url;
+            const title = rssItem.title;
+            const publishDate = rssItem.isoDate;
+            if (await isExistingThreadDmhyLink(dmhyLink)) {
+              return;
+            }
 
-          await bindSubscriptionAndThread(sub.id, threadResult.lastID);
-          debug('dmhy:cli:pull:rssItem')(rssItem);
+            const threadResult = await createThread(dmhyLink, magnet, title, publishDate);
+            debug('dmhy:cli:pull:threadResult')(threadResult);
 
-          logger.log(`pull 「${title}」`);
-        }));
-      }),
+            await bindSubscriptionAndThread(sub.id, threadResult.lastID);
+            debug('dmhy:cli:pull:rssItem')(rssItem);
+
+            logger.log(`pull 「${title}」`);
+          }));
+        }),
     );
   } catch (err) {
     debug('dmhy:cli:pull')(err);
