ad86e110388a85121454b7cf88279a9895bb8b70
FlandreDaisuki <vbnm123c+github@gmail.com>
2023-05-29 08:58:12 +0800

    feat: expose `ARIA2_HOST` env

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dmhy config downloader aria2
-dmhy config aria2-jsonrpc "http://token:${ARIA2_SECRET}@aria2:${ARIA2_PORT}/jsonrpc"
+dmhy config aria2-jsonrpc "http://token:${ARIA2_SECRET}@${ARIA2_HOST}:${ARIA2_PORT}/jsonrpc"
 
 echo 'Start running dmhy services...'
 echo "${CRON_FREQ} $(which dmhy) pull --then-download 2>&1 | tee -a /root/app/cron.log" > /var/spool/cron/crontabs/root
